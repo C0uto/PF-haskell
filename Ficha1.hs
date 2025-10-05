07f40b997e29a628fa80df2f34324aeba56a274e
Nuno Couto <n.miguel2001@gmail.com>
2025-10-05 17:14:50 +0100

    mais uns exercicios

--- Ficha1.hs
+++ Ficha1.hs
@@ -30,3 +30,56 @@ max3 (n,m,x) = if n >= m && n >= x
                         else x
 
 -- Exercício 2
+nRaizes :: Double -> Double -> Double -> Int
+nRaizes a b c
+    | d > 0     = 2
+    | d == 0    = 1
+    | otherwise = 0
+    where d = b^2 - 4*a*c
+
+raizes :: Double -> Double -> Double -> [Double]
+raizes a b c
+    | n == 2    = [(-b + sqrt d) / (2*a), (-b - sqrt d) / (2*a)]
+    | n == 1    = [(-b) / (2*a)]
+    | otherwise = []
+    where
+        d = b^2 - 4*a*c
+        n = nRaizes a b c
+
+-- Exercício 3
+type Hora = (Int, Int)
+
+horaValida :: Hora -> Bool
+horaValida (h,m) = h > 0 && h <= 23 && m > 0 && m <= 59
+
+compararHoras :: Hora -> Hora -> Bool
+compararHoras (h1,m1) (h2,m2) = h1 > h2 || (h1 == h2 && m1 > m2)
+
+converterHoras :: Hora -> Int
+converterHoras (h,m) = h*60 + m
+
+converterMinutos :: Int -> Hora
+converterMinutos m = (div m 60, mod m 60)
+
+diferencaHoras :: Hora -> Hora -> Int
+diferencaHoras (h1,m1) (h2,m2) = converterHoras (h1,m1) - converterHoras (h2,m2)
+
+-- Exercício 5
+data Semaforo = Verde | Amarelo | Vermelho deriving (Show, Eq)
+
+next :: Semaforo -> Semaforo
+next Verde = Amarelo
+next Amarelo = Vermelho
+next Vermelho = Verde
+
+stop :: Semaforo -> Bool
+stop Vermelho = True
+stop _ = False
+
+safe :: Semaforo -> Semaforo -> Bool
+safe Vermelho _ = True
+safe _ Vermelho = True
+safe _ _ = False
+
+-- Exercício 6
+data Ponto = Cartesiano Double Double | Polar Double Double deriving (Show, Eq)
